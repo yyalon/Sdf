da31330d40a5f83d2e123e3bbbaf51365554672d
杨云鹏 <z331047418@aliyun.com>
2020-08-22 15:58:21 +0800

    update

--- src/Sdf/Fundamentals/Json/DatetimeJsonConverter.cs
+++ src/Sdf/Fundamentals/Json/DatetimeJsonConverter.cs
@@ -14,6 +14,10 @@ namespace Sdf.Fundamentals.Json
             {
                 if (DateTime.TryParse(reader.GetString(), out DateTime date))
                     return date;
+                else
+                {
+                    return new DateTime();
+                }
             }
             return reader.GetDateTime();
         }
@@ -22,6 +26,34 @@ namespace Sdf.Fundamentals.Json
         {
             //writer.WriteStringValue(value.ToString("yyyy-MM-dd HH:mm:ss"));
             writer.WriteStringValue(value.ToString("yyyy-MM-ddTHH:mm:sszzz"));
+        }
+    }
+    public class DatetimeAvailableNullJsonConverter : JsonConverter<DateTime?>
+    {
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                if (DateTime.TryParse(reader.GetString(), out DateTime date))
+                    return date;
+                else
+                {
+                    return null;
+                }
+            }
+            return reader.GetDateTime();
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            if (value.HasValue)
+            {
+                writer.WriteStringValue(value.Value.ToString("yyyy-MM-ddTHH:mm:sszzz"));
+            }
+            else
+            {
+                writer.WriteStringValue(string.Empty);
+            }
         }
     }
 }
