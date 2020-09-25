667cb196cba6c641968aaa456d5d9c423a389073
杨云鹏 <z331047418@aliyun.com>
2020-09-25 19:23:35 +0800

    Create Utils.cs

---
+++ src/Sdf.Common/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Sdf.Common
+{
+    public class Utils
+    {
+        public static string Combine(params string[] paths)
+        {
+            string path = Path.Combine(paths);
+            return path.Replace("\\", "/");
+        }
+    }
+}
