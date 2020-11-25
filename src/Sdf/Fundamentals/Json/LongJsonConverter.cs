b3d0aabbde389669e1db32467c6821daa71e5276
杨云鹏 <z331047418@aliyun.com>
2020-11-25 18:32:59 +0800

    Update LongJsonConverter.cs

--- src/Sdf/Fundamentals/Json/LongJsonConverter.cs
+++ src/Sdf/Fundamentals/Json/LongJsonConverter.cs
@@ -42,4 +42,40 @@ namespace Sdf.Fundamentals.Json
 
         }
     }
+
+    public class LongAvailableNullJsonConverter : JsonConverter<long?>
+    {
+        public override long? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.Number)
+            {
+                return reader.GetInt64();
+            }
+            string longValue = reader.GetString();
+
+            if (string.IsNullOrEmpty(longValue))
+            {
+                return null;
+            }
+            return Convert.ToInt64(longValue);
+        }
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="writer"></param>
+        /// <param name="value"></param>
+        /// <param name="options"></param>
+        public override void Write(Utf8JsonWriter writer, long? value, JsonSerializerOptions options)
+        {
+            if (value != 0)
+            {
+                writer.WriteStringValue(value.ToString());
+            }
+            else
+            {
+                writer.WriteStringValue(String.Empty);
+            }
+
+        }
+    }
 }
