f700448775c58270287440f1302bcb2b735edb3d
杨云鹏 <z331047418@aliyun.com>
2021-08-15 15:25:01 +0800

    Update EntityLongPrimaryAutoKeyUpdatetime.cs

--- src/Sdf.EF/Entities/EntityLongPrimaryAutoKeyUpdatetime.cs
+++ src/Sdf.EF/Entities/EntityLongPrimaryAutoKeyUpdatetime.cs
@@ -7,6 +7,9 @@ namespace Sdf.EF.Entities
 {
     public class EntityLongPrimaryAutoKeyUpdatetime : EntityLongPrimaryKeyAuto, IUpdateTimeField
     {
+        /// <summary>
+        /// 最后更新时间
+        /// </summary>
         public DateTime UpdateTime { get; set; }
     }
 }
